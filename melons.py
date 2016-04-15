a9edb6745b79bbc5d89822e0d184944767f7ec26
Hackbright Student <info@hackbrightacademy.com>
2016-04-15 13:08:26 -0700

    Exception works

--- melons.py
+++ melons.py
@@ -1,6 +1,14 @@
 """This file should have our order classes in it."""
+from random import randint 
 
+class TooManyMelons(ValueError):
+    ''' '''
 
+    def __init__(self):
+        super(TooManyMelons,self).__init__()
+
+    def __str__(self):
+        return "Too many melons!"
 
 class AbstractMelonOrder(object):
     """ We assume a domestic order unless specified """
@@ -8,24 +16,47 @@ class AbstractMelonOrder(object):
     def __init__(self, species, qty, tax, order_type, country_code):
         """ Creation of a initialization method that sets parameters to attributes """
         self.species = species
-        self.qty = qty
+        try:
+            if qty <100:
+                self.qty = qty
+            else:
+                raise TooManyMelons()
+        except TooManyMelons as e:
+            print e
+
         self.shipped = False
         self.order_type = order_type
         self.tax = tax
         self.country_code = country_code
+        self.base_price = 0
 
-    def get_total(self, surge_multiplier = 1, shipping_flat_fee = 0):
+    def get_total(self, holiday_multiplier = 1, shipping_flat_fee = 0):
         """Calculate price."""
 
-        base_price = 5 
-        price = base_price * holiday_multiplier
+        self.get_base_price()
+        price = self.base_price * holiday_multiplier
         total = (1 + self.tax) * self.qty * price + shipping_flat_fee
         return total
+
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
+
 
     def mark_shipped(self):
         """Set shipped to true."""
 
         self.shipped = True
+
+    def get_base_price(self):
+        """ Checks to see if base price has been set.
+            If not, base price is a random val n/w 5-9 (splurge pricing) """
+        if self.base_price == 0:
+            # self.base_value instance attribute 
+            # (initialized in __init__) is rebound.
+            self.base_price = randint(5,9)
 
 
 class DomesticMelonOrder(AbstractMelonOrder):
@@ -47,16 +78,17 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty, country_code):
         super(InternationalMelonOrder, self).__init__(species, qty, self.tax, "international", country_code)
 
-    def get_country_code(self):
-        """Return the country code."""
-
-        return self.country_code
 
     def get_total(self):
-        return super(InternationalMelonOrder, self).get_total(shipping_flat_fee = 3)
+        shipping_fee = 0
+        if self.qty < 10 :
+            shipping_fee = 3
+        return super(InternationalMelonOrder, self).get_total(
+            shipping_flat_fee = shipping_fee)
 
 
 class GovernmentMelonOrder(DomesticMelonOrder):
+    """A domestic (in the US) melon order for the government."""
     
     tax = 0
 
@@ -65,4 +97,6 @@ class GovernmentMelonOrder(DomesticMelonOrder):
         super(GovernmentMelonOrder, self).__init__(species, qty, tax = self.tax )
 
     def mark_inspection(self, passed):
-        self.passed_inspection = True
+        """ updates the status of the melon's passed_inspection
+            passing status. passed is a boolean """
+        self.passed_inspection = passed
